106456ea5854184a438dd8a065f8017ad29e9099
unknown <theunixone@gmail.com>
2022-12-04 19:54:52 -0700

    02-JS-I/homework/homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'Hola Mundox2o';
+const nuevaString = 'Hola Mundo';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
